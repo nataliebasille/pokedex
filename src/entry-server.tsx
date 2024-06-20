ae7a49fb868c59e306b3eb67b235bd5e385d14bb
Natalie Basille <natalie.basille1@gmail.com>
2024-06-19 20:39:58 -0400

    add natcore styles

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,18 +1,23 @@
 // @refresh reload
-import { createHandler, StartServer } from "@solidjs/start/server";
+import { createHandler, StartServer } from '@solidjs/start/server';
 
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang='en'>
         <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <meta charset='utf-8' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+          <style type='text/css'>{`
+            body {
+               display: none;
+            }
+          `}</style>
           {assets}
         </head>
         <body>
-          <div id="app">{children}</div>
+          <div id='app'>{children}</div>
           {scripts}
         </body>
       </html>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
+import natcore from '@natcore/design-system-core/src/plugin';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [natcore()]
 };
